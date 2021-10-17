d659a0bd4e416c28ba4b17ea5814795c25568f40
Tonalli López <aguilablanca_7@live.com>
2021-10-16 19:01:02 -0500

    FEAT: Add environment variables

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
+// Dependencies
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const { createAdapter } = require("@socket.io/redis-adapter");
 const { createClient } = require("redis");
 
+// Environment variables
+require("dotenv").config();
+
+const port = process.env.PORT || 4000;
+const redis_url = process.env.NODE_ENV === "development" ? process.env.REDIS_URL : process.env.REDIS_TLS_URL;
+
+// Routes and sockets
 const routerApi = require("./src/routes");
 const socketApi = require("./src/sockets");
 
+// Middlewares
 const { logErrors, errorHandler, boomErrorHandler } = require("./src/middlewares/error.handler");
 
-const port = 4000;
+// Express
 const app = express();
 const httpServer = createServer(app);
+// Socket.io
 const io = new Server(httpServer, {
   transports: ["websocket"],
   cors: {
     origin: ["*"]
   }
 });
-const pubClient = createClient({ host: "localhost", port: 6379 });
+// Redis
+const pubClient = createClient(redis_url);
 const subClient = pubClient.duplicate();
 
+// Socket.io adapters
 io.adapter(createAdapter(pubClient, subClient));
 
+// Middlewares executions
 app.use(express.json());
 
+// Routes and sockets executions
 app.get("/home", (req, res) => {
   res.send("Hello world");
 });
@@ -32,10 +46,12 @@ app.get("/home", (req, res) => {
 routerApi(app);
 socketApi(io, pubClient);
 
+// Middlewares errors executions
 app.use(boomErrorHandler);
 app.use(logErrors);
 app.use(errorHandler);
 
+// Server on
 httpServer.listen(port, () => {
   console.log(`Server at port: ${port}`);
 });
