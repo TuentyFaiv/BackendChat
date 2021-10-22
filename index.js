13cd7412df5e48fba09c2b6fff956d6bade32181
Tonalli López <aguilablanca_7@live.com>
2021-10-22 17:43:22 -0500

    🔧 REFACTOR: Add cors to REST and sockets

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Dependencies
 const tls = require("tls");
+const cors = require("cors");
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
@@ -11,7 +12,10 @@ require("dotenv").config();
 
 const dev = process.env.NODE_ENV === "development";
 const port = process.env.PORT || 4000;
-const redis_url = dev ? process.env.REDIS_URL : process.env.REDIS_TLS_URL;
+const redisTls = process.env.REDIS_TLS === "true";
+const redisUrl = !redisTls ? process.env.REDIS_URL : process.env.REDIS_TLS_URL;
+const whitelist = process.env.WHITE_LIST.toString().split(",");
+const whitelistSocket = process.env.WHITE_LIST_SOCKETS.toString().split(",");
 
 // Routes and sockets
 const routerApi = require("./src/routes");
@@ -21,21 +25,36 @@ const socketApi = require("./src/sockets");
 const { logErrors, errorHandler, boomErrorHandler } = require("./src/middlewares/error.handler");
 
 // Express
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("not allowed"));
+    }
+  }
+};
 const app = express();
 const httpServer = createServer(app);
 // Socket.io
 const io = new Server(httpServer, {
   transports: ["websocket"],
-  cors: {
-    origin: ["*"]
+  allowRequest: (req, callback) => {
+    if (whitelistSocket.includes(req.headers.origin) || !req.headers.origin) {
+      callback(null, true);
+    } else {
+      callback("not allowed", false);
+    }
   }
 });
 // Redis
-const pubClient = createClient(redis_url, dev ? {} : {
+const redisLocal = { host: "localhost", port: 6379 };
+const redisConfig = dev ? {} : {
   tls: tls.connect(port, {
     enableTrace: true
   })
-});
+};
+const pubClient = dev ? createClient(redisLocal) : createClient(redisUrl, redisConfig);
 const subClient = pubClient.duplicate();
 
 // Socket.io adapters
@@ -43,6 +62,7 @@ io.adapter(createAdapter(pubClient, subClient));
 
 // Middlewares executions
 app.use(express.json());
+app.use(cors(corsOptions));
 
 // Routes and sockets executions
 app.get("/home", (req, res) => {
